8d3866c24e87dc9f00dfc98838542f2e9ba72b64
eM dash <90864557+3mda5h@users.noreply.github.com>
2022-02-05 00:01:34 +0000

    backup

--- .ccls-cache/@home@runner@ConsoleWordle/main.cpp
+++ .ccls-cache/@home@runner@ConsoleWordle/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <map>
+#include <fstream>
 
 using namespace std;
 
@@ -15,30 +17,54 @@ bool charIsCorrect(char c, int index, char* wordle);
 int charCount(char* array, char c);
 int coloredCharCount(Guess* guess, char c);
 void printGuess(Guess* guess);
+void setWordLength(int &wordlen);
+void numbers(int wordlen);
 
 int main() 
 {
+  map<int, char*>dictonary;
+  ifstream file("popular.txt");
+  if(file.is_open())
+  {
+    int i = 0;
+    while(file.good())
+    {
+      file >> dictonary.at(i); 
+    }
+  }
+  
   char wordle[100];
-  char guess[100];
+  char input[100];
   vector<Guess*>guesses;
   bool playing = true;
+  int wordlen;
   while(playing == true)
   {
-    cout << "Enter the wordle: ";
-    cin.getline(wordle, 100);
-    while(strcmp(guess, wordle) != 0)
+    setWordLength(wordlen);
+    int i = 1;
+    while(strlen(dictonary.at(i)) != wordlen)
     {
-      cout << "----------------" << endl;
+      srand(time(NULL));
+      i = rand()%6 + 1;
+    }
+    strcpy(wordle, dictonary.at(i));
+    cout << "wordle is: " << wordle << endl;
+    //cout << "Enter the wordle: ";
+    //cin.getline(wordle, 100);
+    while(strcmp(input, wordle) != 0)
+    {
       bool validGuess = false;
       while(validGuess == false)
       {
+        cout << "-------";
+        numbers(wordlen);
         cout << "Guess: ";
-        cin.getline(guess, 100);
-        if(strlen(guess) > strlen(wordle))
+        cin.getline(input, 100);
+        if(strlen(input) > strlen(wordle))
         {
           cout << "Too long!" << endl;
         }
-        else if(strlen(guess) < strlen(wordle))
+        else if(strlen(input) < strlen(wordle))
         {
           cout << "Too short!" << endl;
         }
@@ -47,14 +73,14 @@ int main()
           validGuess = true;
         }
       }
-      cout << "----------------" << endl;
+      cout << "-----------------" << endl;
       Guess* newGuess = new Guess();
-      strcpy(newGuess->chars, guess);
+      strcpy(newGuess->chars, input);
       guesses.push_back(newGuess); 
       //set all the greens
-      for(int i = 0; i < strlen(guess); i++)
+      for(int i = 0; i < strlen(input); i++)
       {
-        if(charIsCorrect(guess[i], i, wordle) == true)
+        if(charIsCorrect(input[i], i, wordle) == true)
         {
           newGuess->colors[i] = 2;
         }
@@ -64,12 +90,12 @@ int main()
         }
       }
       //set any yellows
-      for(int i = 0; i < strlen(guess); i++)
+      for(int i = 0; i < strlen(input); i++)
       {
         //for debugging:
         /*cout << "number of " << guess[i] << " that are green or yellow: " << usedCharCount(newGuess, guess[i]) << endl; 
         cout << "number of " << guess[i] <<  " in wordle: " << charCount(wordle, guess[i]) << endl;*/
-        if(newGuess->colors[i] != 2 && coloredCharCount(newGuess, guess[i]) < charCount(wordle, guess[i])) //if char is not green and the amount of yellow/green ones of this char currently in guess is less than the amount of this char in wordle
+        if(newGuess->colors[i] != 2 && coloredCharCount(newGuess, input[i]) < charCount(wordle, input[i])) //if char is not green and the amount of yellow/green ones of this char currently in guess is less than the amount of this char in wordle
         {
           newGuess->colors[i] = 1;
         }
@@ -85,17 +111,47 @@ int main()
         printGuess(guesses[i]);
       }
     }
-    cout << "----------------" << endl;
+    guesses.clear();
+    cout << "-----------------" << endl;
     cout << "You win!" << endl;
     cout << "Total guesses: " << guesses.size() << endl;
     cout << "Play again? (y/n)" << endl;
     cout << "> ";
-    cin.getline(guess, 100);
-    if(strcmp(guess, "n") == 0)
+    cin.getline(input, 100);
+    if(strcmp(input, "n") == 0)
     {
       playing = false;
     }   
   }
+}
+
+void setWordLength(int &wordlen)
+{
+  char input[100];
+  bool validInput = false;
+  while(validInput == false)
+  {
+    cout << "Enter word length: ";
+    cin.getline(input, 100);
+    if(atoi(input) > 9 || atoi(input) < 3)
+    {
+      cout << "Not a valid length" << endl;
+    }
+    else
+    {
+      validInput = true;
+      wordlen = atoi(input);
+    }
+  }
+}
+
+void numbers(int wordlen)
+{
+  for(int i = 1; i <= wordlen; i++)
+  {
+    cout << i;
+  }
+  cout << "--" << endl;
 }
 
 //wordle matches given char at same index 
